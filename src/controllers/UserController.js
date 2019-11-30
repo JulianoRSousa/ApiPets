061dd9404f4756b2e3a46276349af22879be8688
unknown <julianorodsousa@gmail.com>
2019-11-30 09:54:08 -0400

    Tryng auth the token on profilePicture method - (3)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -35,12 +35,9 @@ module.exports = {
         const { profilePicture } = req.file;
         const { token } = req.headers;
         let user = null;
-        const authent = await Auth.findOne({ _id: token }).then(Response => {})
-        console.log("Authent = ", authent)
+        
         await Auth.findOne({ _id: token }).then(Response => {
-            console.log("response = ", Response),
-            console.log("response.data = ", Response.data),
-            console.log("response.data.user = ", Response.data.user),
+            console.log("response.user = ", Response.user),
             user = Response.data.user
         })
 
