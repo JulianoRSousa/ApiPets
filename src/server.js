b35aa4b7a87538e100b506fe784e6c1f88360e77
Juliano Sousa <julianorodsousa@gmail.com>
2020-08-27 18:35:24 -0400

    Update server.js
    Trying connect mongodb 2

--- src/server.js
+++ src/server.js
@@ -8,7 +8,9 @@ const routes = require('./routes');
 
 const app = express();
 try {
-    mongoose.connect('mongodb://petspets:petspets@petsdb-shard-00-00-i0iht.mongodb.net:27017,petsdb-shard-00-01-i0iht.mongodb.net:27017,petsdb-shard-00-02-i0iht.mongodb.net:27017/test?ssl=true&replicaSet=PetsDB1-shard-0&authSource=admin&retryWrites=true&w=majority',{
+    
+    mongoose.connect('mongodb://petspets:petspets@petsdb1-shard-00-00.i0iht.gcp.mongodb.net:27017,petsdb1-shard-00-01.i0iht.gcp.mongodb.net:27017,petsdb1-shard-00-02.i0iht.gcp.mongodb.net:27017/test?replicaSet=PetsDB1-shard-0&ssl=true&authSource=admin',{
+    //mongoose.connect('mongodb://petspets:petspets@petsdb-shard-00-00-i0iht.mongodb.net:27017,petsdb-shard-00-01-i0iht.mongodb.net:27017,petsdb-shard-00-02-i0iht.mongodb.net:27017/test?ssl=true&replicaSet=PetsDB1-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
