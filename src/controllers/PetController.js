d4daf1053904278033e12d80ff2f655c553aab34
Juliano Sousa <julianorodosusa@gmail.com>
2020-09-10 00:28:34 -0400

    Pet Methods fixed //OK//

--- src/controllers/PetController.js
+++ src/controllers/PetController.js
@@ -6,16 +6,24 @@ module.exports = {
     async getPetByUserId(req, res) {
         const { user_id } = req.headers;
 
-        const pets = await Pet.find({ user: user_id });
-
-        return res.json(pets);
+        try {
+            const pets = await Pet.find({ user: user_id });
+            return res.status(200).json(pets);
+        } catch (error) {
+            return res.status(500).json({ "Error": "Invalid User Format" });
+        }
     },
 
     async showallpets(req, res) {
-
-        const pets = await Pet.find();
-
-        return res.json(pets);
+        if (process.env.ENVIRONMENT == 'dev') {
+            try {
+                const pets = await Pet.find();
+                return res.status(200).json(pets);
+            } catch (error) {
+                return res.status(403).json({ 'Error': 'Cant Find Pets' });
+            }
+        }
+        return res.status(403).json({ "Error": "No system admin logged" });
     },
 
     async store(req, res) {
@@ -29,40 +37,46 @@ module.exports = {
             male,
         } = req.body;
         const { token } = req.headers;
-        const authenticated = await Auth.findOne({ _id: token });
 
-
-        if (authenticated) {
-            const pet = await Pet.create({
-                profilePicture: profilePicture,
-                pictures: "",
-                status: "Neutral",
-                firstName: firstName,
-                lastName: lastName,
-                color: color,
-                coatSize: coatSize,
-                birthdate: birthdate,
-                male: male,
-                user: authenticated.user,
-            })
-            return res.json(pet);
+        try {
+            const authenticated = await Auth.findOne({ _id: token });
+            if (authenticated) {
+                const pet = await Pet.create({
+                    profilePicture: profilePicture,
+                    pictures: "",
+                    status: "Neutral",
+                    firstName: firstName,
+                    lastName: lastName,
+                    color: color,
+                    coatSize: coatSize,
+                    birthdate: birthdate,
+                    male: male,
+                    user: authenticated.user,
+                })
+                return res.status(201).json(pet);
+            }
+            return res.status(401).json({ 'Error': 'Invalid Token' });
+        } catch (error) {
+            return res.status(500).json({ 'Error': 'Invalid Token Format' });
         }
-        return res.json({ 'error': 'Invalid Token' });
+
+
+
     },
 
     async deletepet(req, res) {
         const { pet, token } = req.headers;
         try {
             const authenticated = await Auth.findOne({ _id: token });
-            if (authenticated.auth) {
+            if (authenticated) {
                 const petData = await Pet.deleteOne({ _id: pet, user: authenticated.user });
-                if (petData.deletedCount != 0)
-                    return res.json(petData);
-                return res.json({ 'error': 'Inappropriete Pet' });
+                if (petData.deletedCount)
+                    return res.status(201).json(petData);
+                return res.status(404).json({ 'error': 'Inappropriete Pet' });
             }
-            return res.json({ 'error': 'Inappropriete User or Pet' });
+            return res.status(401).json({ 'error': 'Inappropriete User or Pet' });
         } catch (error) {
-            return res.json({ 'error': 'Invalid token parameters' });
+            return res.status(500).json({ 'error': 'Invalid token parameters' });
         }
     },
 
