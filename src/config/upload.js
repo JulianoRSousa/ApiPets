b72b109a3c1bf744ebad02f630bb91cf4cb6ed16
julianorodsousa <julianorodsousa@gmail.com>
2020-09-29 08:37:01 -0400

    Starting now the IBM Cloud Configurations

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,5 +1,45 @@
 const multer = require('multer');
 const path = require('path');
+const crypto = require("crypto");
+const aws = require('ibm-cos-sdk');
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+
+const ep = new aws.Endpoint('s3.us-south.cloud-object-storage.appdomain.cloud');
+const s3 = new aws.S3({ endpoint: ep, region: 'us-south-1' });
+
+
+const storageTypes = {
+    local: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
+        },
+        filename: (req, file, cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err);
+
+                file.key = `${hash.toString("hex")}-${file.originalname}`;
+
+                cb(null, file.key);
+            });
+        }
+    }),
+    s3: multerS3({
+        s3: s3,
+        bucket: process.env.BUCKET_NAME,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: "public-read",
+        key: (req, file, cb) => {
+            crypto.randomBytes(16, (err, hash) => {
+                if (err) cb(err);
+
+                const fileName = `${hash.toString("hex")}-${file.originalname}`;
+
+                cb(null, fileName);
+            });
+        }
+    })
+};
 
 module.exports = {
     storage: multer.diskStorage({
@@ -7,8 +47,9 @@ module.exports = {
         filename: (req, file, cb) => {
             const ext = path.extname(file.originalname);
             const name = path.basename(file.originalname, ext);
-            
+
             cb(null, `${name}-${Date.now()}${ext}`);
         }
     }),
 }
+
