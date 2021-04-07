43a706483f5f4c76b9c4a6a8f4919b66d442bba7
julianorodsousa <julianorodsousa@gmail.com>
2021-04-07 11:53:08 -0400

    getPostByUserId set to get posts

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -15,9 +15,33 @@ module.exports = {
 
   async getPostByUserId(req, res) {
     try {
-      const { user_id } = req.headers;
-      const post = await Post.find({ user: user_id });
-      return res.status(200).json(post);
+      const { token, user_id } = req.headers;
+      const auth = Auth.findOne({ _id: token });
+      if (auth) {
+        const posts = await Post.find({user: auth.user._id})
+          .populate({ path: "user" })
+          .populate({ path: "pet" });
+
+        var postList = [];
+        for (var i = 0; i < posts.length; i++) {
+          postList[i] = {
+            post_id: posts[i]._id,
+            post_picture: posts[i].picture_url,
+            post_status: posts[i].state,
+            post_description: posts[i].description,
+            post_date: posts[i].postDate,
+            post_time: posts[i].postTime,
+            pet_name: posts[i].pet.firstName,
+            pet_picture: posts[i].pet.picture_url,
+            pet_id: posts[i].pet._id,
+            user_id: posts[i].user._id,
+            user_name: posts[i].user.username,
+            user_picture: posts[i].user.picture_url,
+          };
+        }
+        return res.status(200).json(postList);
+      }
+      return status(403).json({ error: "Invalid User" });
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
