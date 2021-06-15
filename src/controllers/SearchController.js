d964b6d3fd0673f9d99d2c6d539a40894d8ad34b
julianorodsousa <julianorodsousa@gmail.com>
2021-06-15 10:08:52 -0400

    EngineSearch (8)

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -5,11 +5,11 @@ const User = require('../models/User');
 
 module.exports = {
 
-    async searchFriends(req, res) {
+    async searchFriends(req, res, next) {
         const { tags } = req.headers;
             try {
                 // const resultSearch1 = await User.find({"tags": { $all: tags[0],tags[1] }}).pretty()
-                const resultSearch2 = await User.find({"tags": { $in: ["DOIS"] }}).pretty()
+                const resultSearch2 = await User.find({"tags": { $in: tags }})
                     await resultSearch2.populate('user').execPopulate();
                     return res.status(200).json(resultSearch2);
             } catch (error) {
