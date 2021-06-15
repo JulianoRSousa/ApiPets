0a077aeef2700fde0842c749c08d8f38b511f1ef
julianorodsousa <julianorodsousa@gmail.com>
2021-06-15 16:56:40 -0400

    getUserById Changed (3)

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -47,7 +47,7 @@ module.exports = {
   async getUserById(req, res) {
     try {
       const { user_id } = req.headers;
-      const user = await User.find({ _id: user_id });
+      const user = await User.findOne({ _id: user_id });
 
       if (user) {
         const pets = await Pet.find({ user: user._id });
