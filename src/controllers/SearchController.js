cbd835af9f69d89afe81d71c93c8f23b890d4f95
julianorodsousa <julianorodsousa@gmail.com>
2021-06-16 16:59:34 -0400

    '@' now determines a username search (3)

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -10,7 +10,7 @@ module.exports = {
     try {
       if (search.indexOf("@") == 0) {
           search[0].replace('@','')
-        const resultSearch = await User.find({ tags: { $all: search } });
+        const resultSearch = await User.find({ tags: { $all: search[0] } });
         return res.status(200).json(resultSearch);
       } else {
         const resultSearch = await User.find({ tags: { $in: search } });
