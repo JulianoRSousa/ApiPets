7d8d74c5a5df1ed52c48637c05fe010588ee052a
julianorodsousa <julianorodsousa@gmail.com>
2021-08-19 10:00:08 -0400

    getData() created - and set User Model - 2

--- src/controllers/DataController.js
+++ src/controllers/DataController.js
@@ -4,9 +4,35 @@ const Post = require("../models/Post");
 const Pet = require("../models/Pet");
 const Follow = require("../models/Follow");
 
-//index, show, store, update, destroy
-
 module.exports = {
+  async getData(req, res) {
+    const { token } = req.headers;
+    try {
+      const authenticated = await Auth.findOne({ _id: token });
+      if (authenticated) {
+        const user = await User.findOne({ _id: authenticated.user });
+        if (user) {
+          const pets = await Pet.find({ user: user._id });
+          const posts = await Post.find({ user: user._id });
+          const following = await Follow.find({ following: user._id });
+          const follower = await Follow.find({ follower: user._id });
+          user.remove(pass);
+          user.postList = posts;
+          user.petList = pets;
+          user.followingList = following;
+          user.followerList = follower;
+        }
+        return res.status(201).json(user);
+      } else {
+        return res.status(401).json({ error: "Invalid Token for this user" });
+      }
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ Error: "Probably Invalid Token" + error.message });
+    }
+  },
+
   // async confirmauth(req, res) {
   //   const { token } = req.headers;
   //   if (token == null || token == "") {
@@ -43,55 +69,26 @@ module.exports = {
   //   return res.status(403).json({ error: "No system admin logged" });
   // },
 
-  async getData(req, res) {
-    const { token } = req.headers;
-    try {
-      const authenticated = await Auth.findOne({ _id: token });
-      if (authenticated) {
-        const user = await User.findOne({ _id: authenticated.user });
-        if (user) {
-          const pets = await Pet.find({ user: user._id });
-          const posts = await Post.find({ user: user._id });
-          const following = await Follow.find({ following: user._id });
-          const follower = await Follow.find({ follower: user._id });
-          user.pass = null;
-          user.postList = posts;
-          user.petList = pets;
-          user.followingList = following;
-          user.followerList = follower;
-          console.log("this is DATA: ", user);
-        }
-        return res.status(201).json(user);
-      } else {
-        return res.status(401).json({ error: "Invalid Token for this user" });
-      }
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ Error: "Probably Invalid Token" + error.message });
-    }
-  },
+  // async deleteauth(req, res) {
+  //   const { token } = req.headers;
+  //   try {
+  //     const auth = await Auth.deleteOne({ _id: token });
+  //     if (auth.deletedCount) return res.status(201).json(auth);
+  //     return res.status(401).json({ Error: "No auth for this token" });
+  //   } catch (error) {
+  //     return res.status(500).json({ "Internal Server Error": error.message });
+  //   }
+  // },
 
-  async deleteauth(req, res) {
-    const { token } = req.headers;
-    try {
-      const auth = await Auth.deleteOne({ _id: token });
-      if (auth.deletedCount) return res.status(201).json(auth);
-      return res.status(401).json({ Error: "No auth for this token" });
-    } catch (error) {
-      return res.status(500).json({ "Internal Server Error": error.message });
-    }
-  },
-
-  async deleteallauth(req, res) {
-    if (process.env.ENVIRONMENT == "dev") {
-      try {
-        await Auth.deleteMany();
-      } catch (error) {
-        return res.status(500).json({ Error: "Error on delete" });
-      }
-      return res.json({ message: "Deleted" });
-    }
-    return res.status(403).json({ error: "No system admin logged" });
-  },
+  // async deleteallauth(req, res) {
+  //   if (process.env.ENVIRONMENT == "dev") {
+  //     try {
+  //       await Auth.deleteMany();
+  //     } catch (error) {
+  //       return res.status(500).json({ Error: "Error on delete" });
+  //     }
+  //     return res.json({ message: "Deleted" });
+  //   }
+  //   return res.status(403).json({ error: "No system admin logged" });
+  // },
 };
